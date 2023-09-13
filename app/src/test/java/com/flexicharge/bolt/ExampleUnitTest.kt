6363861808d4d611951e94e74ffb628a0a30cd55
Talal <92815664+TGH97@users.noreply.github.com>
2023-09-13 10:17:26 +0200

    ::

--- app/src/test/java/com/flexicharge/bolt/ExampleUnitTest.kt
+++ app/src/test/java/com/flexicharge/bolt/ExampleUnitTest.kt
@@ -1,10 +1,14 @@
 package com.flexicharge.bolt
 
+import androidx.test.core.app.ActivityScenario.launch
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.flexicharge.bolt.activities.MainActivity
 import com.flexicharge.bolt.api.flexicharge.RetrofitInstance
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -39,6 +43,57 @@ class ExampleUnitTest {
         val response = RetrofitInstance.flexiChargeApi.getTransaction(1)
         assert(response.isSuccessful)
     }
+
+    @RunWith(AndroidJUnit4::class)
+    class TestMainActivity {
+        @Test fun testUnixToDateTimeIsCorrect() {
+            val activityScenario = launch(MainActivity::class.java)
+            activityScenario.onActivity { activity ->
+                val unixTime = 0
+                val dateTime = activity.unixToDateTime(unixTime.toString())
+                println("Unix time: %s, dateTime: %s".format(unixTime.toString(), dateTime))
+                assert(dateTime == "01/01/00:00")
+            }
+        }
+
+        @Test fun testValidateChargerIdIsCorrect() {
+            val activityScenario = launch(MainActivity::class.java)
+
+            fun getIdString(id: Int): String {
+                val ones = id % 10
+                val tens = (id / 10) % 10
+                val hundreds = (id / 100) % 10
+                val thousands = (id / 1000) % 10
+                val tenThousands = (id / 10000) % 10
+                val hundredThousands = (id / 100000) % 10
+
+                return "%d%d%d%d%d%d".format(
+                    hundredThousands,
+                    tenThousands,
+                    thousands,
+                    hundreds,
+                    tens,
+                    ones
+                )
+            }
+            activityScenario.onActivity { activity ->
+                for(i in 0 .. 999999) {
+                    val idString = getIdString(i)
+                    assert(activity.validateChargerId(idString))
+                }
+
+                assert(!activity.validateChargerId("-00000"))
+                assert(!activity.validateChargerId("00000000"))
+                assert(!activity.validateChargerId("0000000"))
+                assert(!activity.validateChargerId("00000"))
+                assert(!activity.validateChargerId("0000"))
+                assert(!activity.validateChargerId("000"))
+                assert(!activity.validateChargerId("00"))
+                assert(!activity.validateChargerId("0"))
+
+            }
+
+        }
 
     @Test
     fun testGetChargePoint() = runBlocking {
